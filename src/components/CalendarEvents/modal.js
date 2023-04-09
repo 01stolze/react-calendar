289ab58aaa6a18f5f6482a5e5e69cffd03d68929
Gustavo <gustavostolzedeandrade@gmail.com>
2023-04-09 20:43:11 -0300

    fixing bugs

--- src/components/CalendarEvents/modal.js
+++ src/components/CalendarEvents/modal.js
@@ -30,30 +30,21 @@ export const getFormData = (
     dia de outro */
 
     const sameDayEvent = () => {
-      for (let i = 0; eventsData.length > i; i++) {
-        if (
-          eventsData[i].title == '' &&
-          eventsData[i].newTitle == '' &&
-          eventsData[i].new3Title == '' &&
-          eventsData[i].new4Title == '' &&
-          eventsData[i].new5Title == ''
-        ) {
-          console.warn(
-            'TODOS EVENTOS DELETADOS DETECTADO AO TENTAR ADICIONAR EVENTO: APAGANDO...',
-          )
-          delete eventsData[i]
-        } else if (date.value == eventsData[i].date) {
-          return i
-        } else if (date.value == eventsData[i].newDate) {
-          return i
-        } else if (date.value == eventsData[i].new3Date) {
-          return i
-        } else if (date.value == eventsData[i].new4Date) {
-          return i
-        } else if (date.value == eventsData[i].new5Date) {
-          return i
+      eventsData.forEach((data) => {
+        if (date.value == data.date) {
+          // fix
+          console.log('aaa', eventsData.indexOf(data))
+          return eventsData.indexOf(data)
+        } else if (date.value == data.newDate) {
+          return eventsData.indexOf(data.newDate)
+        } else if (date.value == data.new3Date) {
+          return eventsData.indexOf(data.new3Date)
+        } else if (date.value == data.new4Date) {
+          return eventsData.indexOf(data.new4Date)
+        } else if (date.value == data.new5Date) {
+          return eventsData.indexOf(data.new5Date)
         }
-      }
+      })
     }
 
     /* tratar múltiplios eventos no
@@ -291,104 +282,90 @@ export const handleEditEvent = (
     const hourEnd = document.querySelector('#end-time')
     console.log('REALMENTE EDITAR OS EVENTOS, noufa garai')
     console.log('evento a ser editado:', editEventsData)
-    for (let i = 0; eventsData.length > i; i++) {
-      // fix
-      if (editEventsData[0].title == eventsData[i].title) {
+    // fix
+
+    eventsData.forEach((data) => {
+      if (editEventsData[0].title == data.title) {
         if (wichEvent == 'delete') {
-          console.warn('removendo title')
-          eventsData[i].title = ''
-          eventsData[i].date = ''
-          eventsData[i].description = ''
-          eventsData[i].hourStart = ''
-          eventsData[i].hourEnd = ''
-          console.log(eventsData, 'eventsdata apos delete')
+          console.warn('removendo o title')
+          data.title = ''
+          data.date = ''
+          data.description = ''
+          data.hourStart = ''
+          data.hourEnd = ''
         } else {
-          eventsData[i] = {
-            ...eventsData[i],
-            title: title.value,
-            date: date.value,
-            description: description.value,
-            hourStart: hourStart.value,
-            hourEnd: hourEnd.value,
-          }
+          console.log('CHEGOU AQUI')
+          data.title = title.value
+          data.date = date.value
+          data.description = description.value
+          data.hourStart = hourStart.value
+          data.hourEnd = hourEnd.value
         }
-      } else if (editEventsData[0].title == eventsData[i].newTitle) {
-        console.warn('removendo newTitle')
+      } else if (editEventsData[0].title == data.newTitle) {
         if (wichEvent == 'delete') {
-          eventsData[i].newTitle = ''
-          eventsData[i].newDate = ''
-          eventsData[i].newDescription = ''
-          eventsData[i].newHourStart = ''
-          eventsData[i].newHourEnd = ''
-          console.log(eventsData, 'eventsdata apos delete')
+          console.warn('removendo o newTitle')
+          data.newTitle = ''
+          data.newDate = ''
+          data.newDescription = ''
+          data.newHourStart = ''
+          data.newHourEnd = ''
         } else {
-          eventsData[i] = {
-            ...eventsData[i],
-            newTitle: title.value,
-            newDate: date.value,
-            newDescription: description.value,
-            newHourStart: hourStart.value,
-            newHourEnd: hourEnd.value,
-          }
+          console.log('CHEGOU AQUI')
+          data.newTitle = title.value
+          data.newDate = date.value
+          data.newDescription = description.value
+          data.newHourStart = hourStart.value
+          data.newHourEnd = hourEnd.value
         }
-      } else if (editEventsData[0].title == eventsData[i].new3Title) {
-        console.warn('removendo new3Title')
+      } else if (editEventsData[0].title == data.new3Title) {
         if (wichEvent == 'delete') {
-          eventsData[i].new3Title = ''
-          eventsData[i].new3Date = ''
-          eventsData[i].new3Description = ''
-          eventsData[i].new3HourStart = ''
-          eventsData[i].new3HourEnd = ''
-          console.log(eventsData, 'eventsdata apos delete')
+          console.warn('removendo o new3Title')
+          data.new3Title = ''
+          data.new3Date = ''
+          data.new3Description = ''
+          data.new3HourStart = ''
+          data.new3HourEnd = ''
         } else {
-          eventsData[i] = {
-            ...eventsData[i],
-            new3Title: title.value,
-            new3Date: date.value,
-            new3Description: description.value,
-            new3HourStart: hourStart.value,
-            new3HourEnd: hourEnd.value,
-          }
+          console.log('CHEGOU AQUI')
+          data.new3Title = title.value
+          data.new3Date = date.value
+          data.new3Description = description.value
+          data.new3HourStart = hourStart.value
+          data.new3HourEnd = hourEnd.value
         }
-      } else if (editEventsData[0].title == eventsData[i].new4Title) {
-        console.warn('removendo o new4Title')
+      } else if (editEventsData[0].title == data.new4Title) {
         if (wichEvent == 'delete') {
-          eventsData[i].new4Title = ''
-          eventsData[i].new4Date = ''
-          eventsData[i].new4Description = ''
-          eventsData[i].new4HourStart = ''
-          eventsData[i].new4HourEnd = ''
-          console.log(eventsData, 'eventsdata apos delete')
+          console.warn('removendo o new4Title')
+          data.new4Title = ''
+          data.new4Date = ''
+          data.new4Description = ''
+          data.new4HourStart = ''
+          data.new4HourEnd = ''
         } else {
-          eventsData[i] = {
-            ...eventsData[i],
-            new4Title: title.value,
-            new4Date: date.value,
-            new4Description: description.value,
-            new4HourStart: hourStart.value,
-            new4HourEnd: hourEnd.value,
-          }
+          console.log('CHEGOU AQUI')
+          data.new4Title = title.value
+          data.new4Date = date.value
+          data.new4Description = description.value
+          data.new4HourStart = hourStart.value
+          data.new4HourEnd = hourEnd.value
         }
-      } else if (editEventsData[0].title == eventsData[i].new5Title) {
-        console.warn('removendo o new5Title')
+      } else if (editEventsData[0].title == data.new5Title) {
         if (wichEvent == 'delete') {
-          eventsData[i].new5Title = ''
-          eventsData[i].new5Date = ''
-          eventsData[i].new5Description = ''
-          eventsData[i].new5HourStart = ''
-          eventsData[i].new5HourEnd = ''
-          console.log(eventsData, 'eventsdata apos delete')
+          console.warn('removendo o new5Title')
+          data.new5Title = ''
+          data.new5Date = ''
+          data.new5Description = ''
+          data.new5HourStart = ''
+          data.new5HourEnd = ''
         } else {
-          eventsData[i] = {
-            ...eventsData[i],
-            new5Title: title.value,
-            new5Date: date.value,
-            new5Description: description.value,
-            new5HourStart: hourStart.value,
-            new5HourEnd: hourEnd.value,
-          }
+          console.log('CHEGOU AQUI')
+          data.new5Title = title.value
+          data.new5Date = date.value
+          data.new5Description = description.value
+          data.new5HourStart = hourStart.value
+          data.new5HourEnd = hourEnd.value
         }
       }
-    }
+    })
   }
 }
