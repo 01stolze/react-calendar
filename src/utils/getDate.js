27e63a34a986a15c7fd14b1df6ecc1963618552f
Gustavo <gustavostolzedeandrade@gmail.com>
2023-04-13 23:22:13 -0300

    translate month func

--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -1,3 +1,44 @@
+export const translateMonth = (month) => {
+  switch (month) {
+    case 'January': {
+      return 'Janeiro'
+    }
+    case 'Fevereiro': {
+      return 'Fevereiro'
+    }
+    case 'March': {
+      return 'Março'
+    }
+    case 'April': {
+      return 'Abril'
+    }
+    case 'May': {
+      return 'Maio'
+    }
+    case 'June': {
+      return 'Junho'
+    }
+    case 'July': {
+      return 'Julho'
+    }
+    case 'August': {
+      return 'Agosto'
+    }
+    case 'September': {
+      return 'Setembro'
+    }
+    case 'October': {
+      return 'Outubro'
+    }
+    case 'November': {
+      return 'Novembro'
+    }
+    case 'December': {
+      return 'Dezembro'
+    }
+  }
+}
+
 /* key gen */
 
 export const keyGen = () => {
